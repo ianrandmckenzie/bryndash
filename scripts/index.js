22c26a695edd356d9625f6bf0f02c3536aaa3980
Ian McKenzie <admin@ianrandmckenzie.com>
2022-10-06 21:32:40 -0700

    Add random background

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,26 @@
-
-function getBackgroundURL(){
-  var settings = {
-    "url": "https://api.unsplash.com/photos/random?client_id=X-dmYeqoKxNvyCdcdsg4OJUOYhhhtQXnYMzF6RDfnNI",
-    "method": "GET",
-    "timeout": 0
-  };
-  var responseObject;
-  $.ajax(settings).done(function (response) {
-    responseObject = response;
+$(document).ready(function(){
+  getBackground().then(function(response){
+    if(typeof response != 'undefined'){
+      console.log(response.urls.full);
+      $('#godBod').css('background-image', 'url(' + response.urls.full + ')');
+    }
   });
-  if(typeof responseObject != 'undefined'){
-    return responseObject.urls.full;
-  }
+});
+
+async function getBackground() {
+  var data;
+  var myHeaders = new Headers();
+  myHeaders.append("Cookie", "ugid=7eccd5f9b75bd50dccdf4a645b0cf7305550298");
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  await fetch("https://api.unsplash.com/photos/random?client_id=X-dmYeqoKxNvyCdcdsg4OJUOYhhhtQXnYMzF6RDfnNI", requestOptions)
+    .then(response => data = response.json())
+    .then(result => data = result)
+    .catch(error => console.log('error', error));
+  return data;
 }
